bc7ad1041c0136c041185a14bbc19c801f60e848
Yousef Taha <tyousef262@gmail.com>
2024-10-29 02:13:35 +0300

    fixed bugs

--- AppRouter.py
+++ AppRouter.py
@@ -12,15 +12,17 @@ def home():
 
 @app.route('/tts', methods=['POST'])
 async def convert_text_to_voice():
-    text = request.headers['text']
-    audio_path, audio_file_name = await synthesize_speech(text=text)
+    data = request.get_json()
+    text = data.get('text')
+
+    audio_path = await synthesize_speech(text=text)
 
     if os.path.exists(audio_path):
         current_directory = os.path.dirname(os.path.abspath(audio_path))
-        return send_file(f"{current_directory}\{audio_file_name}", as_attachment=True, download_name="generated_audio.mp3")
+        return send_file(f"{audio_path}", as_attachment=True, download_name="generated_audio.mp3")
     else:
         return jsonify({'error': 'Failed to generate audio'}), 500
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5002)
